5f029d5341f90c3b73d5113b573e1c548a8c0910
Remi Rampin <remirampin@gmail.com>
2014-07-10 18:32:56 -0400

    Factors run_lines in travis scripts

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+run_lines(){
+    while read line; do echo "$line"; sh -c "$line" || exit $?; done
+}
+
 case "$TEST_MODE"
 in
     run_tests)
-        (while read line; do echo "$line"; sh -c "$line" || exit $?; done)<<'EOF'
+        run_lines<<'EOF'
         if [ $TRAVIS_PYTHON_VERSION = "2.6" ]; then pip install unittest2; fi
         python setup.py install
 EOF
--- .travis/test.sh
+++ .travis/test.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+run_lines(){
+    while read line; do echo "$line"; sh -c "$line" || exit $?; done
+}
+
 case "$TEST_MODE"
 in
     run_tests)
-        (while read line; do echo "$line"; sh -c "$line" || exit $?; done)<<'EOF'
+        run_lines<<'EOF'
         python tests
 EOF
         ;;
     check_style)
-        (while read line; do echo "$line"; sh -c "$line" || exit $?; done)<<'EOF'
+        run_lines<<'EOF'
         flake8 --ignore=E126 rpaths.py setup.py tests
 EOF
         ;;
